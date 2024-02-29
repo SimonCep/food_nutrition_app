9d4d4c2ba302787d24089ddcfc42bb2122194dac
SimonCep <103537480+SimonCep@users.noreply.github.com>
2024-02-29 22:17:23 +0200

    Fixed Master

--- food-app/src/app/api/test+api.ts
+++
@@ -1,7 +0,0 @@
-import { ExpoRequest, ExpoResponse } from "expo-router/server";
-
-export function GET(request: ExpoRequest) {
-  return ExpoResponse.json({ hi: "Hello World!!" });
-}
-// TODO: Norint greit pratestuoti API galima padaryt taip: curl localhost:8081/api/test
-//        - Bet geresnis variantas yra yra naudot postmana. https://www.postman.com/
