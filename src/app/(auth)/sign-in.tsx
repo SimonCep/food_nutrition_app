a1049be2c0e50ef33d19090f6230bbcbbd9b5ca5
Dovydas Šiurkus <dovydas.siurkus@ktu.edu>
2024-03-14 17:12:08 +0200

    Made sign up and sign in screens more beautiful

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -15,22 +15,20 @@ const SignInScreen = () => {
       <View className="flex-1 p-5 justify-center">
         <Stack.Screen options={{ title: 'Sign in', headerTitleAlign: 'center' }} />
         <View className="bg-white p-8 rounded-xl shadow-md">
-          <Text className="text-gray-500 text-lg mb-2">Email</Text>
           <TextInput
             value={email}
             onChangeText={setEmail}
-            placeholder="labaskrabas@gmail.com"
-            className="border border-gray-400 p-3 mt-1 mb-4 bg-white rounded-md text-lg"
+            placeholder="Email"
+            className="border-2 border-black p-3 mt-1 mb-4 bg-white rounded-md text-lg"
           />
-          <Text className="text-gray-500 text-lg mb-2">Password</Text>
           <TextInput
             value={password}
             onChangeText={setPassword}
-            placeholder=""
-            className="border border-gray-400 p-3 mt-1 mb-6 bg-white rounded-md text-lg"
+            placeholder="Password"
+            className="border-2 border-black p-3 mt-1 mb-6 bg-white rounded-md text-lg"
             secureTextEntry
           />
-          <TouchableOpacity onPress={handleSignIn} className="bg-yellow-400 py-3 rounded-full mb-4">
+          <TouchableOpacity onPress={handleSignIn} className="bg-yellow-400 py-3 rounded-full border-2 border-black">
             <Text className="text-black font-bold text-lg text-center">Sign in</Text>
           </TouchableOpacity>
           <Link href="/sign-up" className="self-center font-bold text-black text-lg">
--- src/app/(auth)/sign-up.tsx
+++ src/app/(auth)/sign-up.tsx
@@ -16,29 +16,26 @@ const SignUpScreen = () => {
       <View className="flex-1 p-5 justify-center">
         <Stack.Screen options={{ title: 'Sign up', headerTitleAlign: 'center' }} />
         <View className="bg-white p-8 rounded-xl shadow-md">
-          <Text className="text-gray-500 text-lg mb-2">Name</Text>
           <TextInput
             value={name}
             onChangeText={setName}
-            placeholder="Krabas"
-            className="border border-gray-400 p-3 mt-1 mb-4 bg-white rounded-md text-lg"
+            placeholder="Name"
+            className="border-2 border-black p-3 mt-1 mb-4 bg-white rounded-md text-lg"
           />
-          <Text className="text-gray-500 text-lg mb-2">Email</Text>
           <TextInput
             value={email}
             onChangeText={setEmail}
-            placeholder="labaskrabas@gmail.com"
-            className="border border-gray-400 p-3 mt-1 mb-4 bg-white rounded-md text-lg"
+            placeholder="Email"
+            className="border-2 border-black p-3 mt-1 mb-4 bg-white rounded-md text-lg"
           />
-          <Text className="text-gray-500 text-lg mb-2">Password</Text>
           <TextInput
             value={password}
             onChangeText={setPassword}
-            placeholder=""
-            className="border border-gray-400 p-3 mt-1 mb-6 bg-white rounded-md text-lg"
+            placeholder="Password"
+            className="border-2 border-black p-3 mt-1 mb-6 bg-white rounded-md text-lg"
             secureTextEntry
           />
-          <TouchableOpacity onPress={handleSignUp} className="bg-yellow-400 py-3 rounded-full mb-4">
+          <TouchableOpacity onPress={handleSignUp} className="bg-yellow-400 py-3 rounded-full border-2 border-black mb-4">
             <Text className="text-black font-bold text-lg text-center">Create account</Text>
           </TouchableOpacity>
           <Link href="/sign-in" className="self-center font-bold text-black text-lg">
