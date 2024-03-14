cd4ec0a6c6b921e0c867b3907ab7511874c9ac32
Dovydas Šiurkus <dovydas.siurkus@ktu.edu>
2024-03-14 17:17:10 +0200

    Routed stuff for navigation

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
 
 export default function AuthLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="sign-in" options={{ title: 'Sign In' }} />
+      <Stack.Screen name="sign-up" options={{ title: 'Sign Up' }} />
+    </Stack>
+  );
 }
