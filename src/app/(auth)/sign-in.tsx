8068495981577b1d9eda37dba57ce8f36bfdaaed
Dominykas <dominykas058@gmail.com>
2024-03-19 21:40:57 +0200

    login

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -1,19 +1,49 @@
-import { View, Text, TextInput, TouchableOpacity, ImageBackground } from 'react-native';
-import React, { useState } from 'react';
-import { Link, Stack } from 'expo-router';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
+import { Link, Stack } from "expo-router";
+import { supabase } from "@/lib/supabase";
 
 const SignInScreen = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleSignIn = () => {
-    // Handle sign in action here
-  };
+  async function handleSignIn() {
+    //   const { error } = await supabase.auth.signUp({
+    //     email,
+    //     password,
+    //   });
+    //   if (error) {
+    //     Alert.alert(error.message);
+    //   }
+
+    const { error } = await supabase.from("food").insert({
+      id: 2,
+      name: "Denmark",
+      nutritional_value: "Bybys tasej",
+      calories: 300,
+    });
+
+    if (error) {
+      Alert.alert(error.message);
+    }
+  }
 
   return (
-    <ImageBackground source={require('../../assets/images/backgroundImage2.png')} className='bg-white flex-1 justify-center resize-y'>
+    <ImageBackground
+      source={require("../../assets/images/backgroundImage2.png")}
+      className="bg-white flex-1 justify-center resize-y"
+    >
       <View className="flex-1 p-5 justify-center">
-        <Stack.Screen options={{ title: 'Sign in', headerTitleAlign: 'center' }} />
+        <Stack.Screen
+          options={{ title: "Sign in", headerTitleAlign: "center" }}
+        />
         <View className="bg-white p-8 rounded-xl shadow-md">
           <TextInput
             value={email}
@@ -28,10 +58,18 @@ const SignInScreen = () => {
             className="border-2 border-black p-3 mt-1 mb-6 bg-white rounded-md text-lg"
             secureTextEntry
           />
-          <TouchableOpacity onPress={handleSignIn} className="bg-yellow-400 py-3 rounded-full border-2 border-black">
-            <Text className="text-black font-bold text-lg text-center">Sign in</Text>
+          <TouchableOpacity
+            onPress={handleSignIn}
+            className="bg-yellow-400 py-3 rounded-full border-2 border-black"
+          >
+            <Text className="text-black font-bold text-lg text-center">
+              Sign in
+            </Text>
           </TouchableOpacity>
-          <Link href="/sign-up" className="self-center font-bold text-black text-lg">
+          <Link
+            href="/sign-up"
+            className="self-center font-bold text-black text-lg"
+          >
             Create an account
           </Link>
         </View>
--- src/app/(auth)/sign-up.tsx
+++ src/app/(auth)/sign-up.tsx
@@ -1,20 +1,51 @@
-import { View, Text, TextInput, TouchableOpacity, ImageBackground } from 'react-native';
-import React, { useState } from 'react';
-import { Link, Stack } from 'expo-router';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
+import { Link, Stack } from "expo-router";
+import { supabase } from "@/lib/supabase";
 
 const SignUpScreen = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleSignUp = () => {
-    // Handle sign up action here
-  };
+  async function handleSignUp() {
+    // const { error } = await supabase.auth.signUp({
+    //   email,
+    //   password,
+    // });
+
+    // if (error) {
+    //   Alert.alert(error.message);
+    // }
+
+    const { error } = await supabase.from("food").insert({
+      id: 3,
+      name: "Denmarkas dasd asd as",
+      nutritional_value: "Bybys asd as  dasd asasdd ",
+      calories: 300,
+    });
+    Alert.alert("zaza");
+    if (error) {
+      Alert.alert(error.message);
+    }
+  }
 
   return (
-    <ImageBackground source={require('../../assets/images/backgroundImage2.png')} className='bg-white flex-1 justify-center resize-y'>
+    <ImageBackground
+      source={require("../../assets/images/backgroundImage2.png")}
+      className="bg-white flex-1 justify-center resize-y"
+    >
       <View className="flex-1 p-5 justify-center">
-        <Stack.Screen options={{ title: 'Sign up', headerTitleAlign: 'center' }} />
+        <Stack.Screen
+          options={{ title: "Sign up", headerTitleAlign: "center" }}
+        />
         <View className="bg-white p-8 rounded-xl shadow-md">
           <TextInput
             value={name}
@@ -35,10 +66,18 @@ const SignUpScreen = () => {
             className="border-2 border-black p-3 mt-1 mb-6 bg-white rounded-md text-lg"
             secureTextEntry
           />
-          <TouchableOpacity onPress={handleSignUp} className="bg-yellow-400 py-3 rounded-full border-2 border-black mb-4">
-            <Text className="text-black font-bold text-lg text-center">Create account</Text>
+          <TouchableOpacity
+            onPress={handleSignUp}
+            className="bg-yellow-400 py-3 rounded-full border-2 border-black mb-4"
+          >
+            <Text className="text-black font-bold text-lg text-center">
+              Create account
+            </Text>
           </TouchableOpacity>
-          <Link href="/sign-in" className="self-center font-bold text-black text-lg">
+          <Link
+            href="/sign-in"
+            className="self-center font-bold text-black text-lg"
+          >
             Sign in
           </Link>
         </View>
