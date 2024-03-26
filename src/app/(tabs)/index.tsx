186aac85f669f5c5cf269fe219248e0d18739a80
Dovydas Šiurkus <dovydas.siurkus@ktu.edu>
2024-03-26 11:38:45 +0200

    Rewritten home page in tailwindcss

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import {
   View,
   Text,
-  StyleSheet,
   ImageBackground,
   TouchableOpacity,
   Image,
@@ -34,73 +33,32 @@ export default function HomePage() {
   return (
     <ImageBackground
       source={require("../../assets/images/gradientBackground.png")}
-      style={styles.background}
+      className="flex-1 items-center justify-center"
     >
-      <View style={styles.container}>
+      <View className="flex-1 items-center justify-center px-5">
         <Image
           source={require("../../assets/images/icon.png")}
-          style={styles.logo}
+          className="w-24 h-24 mb-5"
         />
-
-        <Text style={styles.title}>Eathy</Text>
-        <Text style={styles.subtitle}>Track your calories and health</Text>
-
-        <TouchableOpacity style={styles.button}>
+        <Text className="text-4xl font-bold text-black mb-2">Eathy</Text>
+        <Text className="text-xl text-black mb-7">
+          Track your calories and health
+        </Text>
+        <TouchableOpacity className="bg-white/50 py-4 px-7 rounded-full mb-4">
           <Link href="./diary">
-            <Text style={styles.buttonText}>Diary</Text>
+            <Text className="text-xl text-black font-bold text-center">
+              Diary
+            </Text>
           </Link>
         </TouchableOpacity>
-
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity className="bg-white/50 py-4 px-7 rounded-full">
           <Link href="./user-info">
-            <Text style={styles.buttonText}>Profile</Text>
+            <Text className="text-xl text-black font-bold text-center">
+              Profile
+            </Text>
           </Link>
         </TouchableOpacity>
       </View>
     </ImageBackground>
   );
 }
-
-const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: 20,
-  },
-  logo: {
-    width: 100,
-    height: 100,
-    marginBottom: 20,
-  },
-  title: {
-    fontSize: 36,
-    fontWeight: "bold",
-    color: "black",
-    marginBottom: 10,
-  },
-  subtitle: {
-    fontSize: 18,
-    color: "black",
-    marginBottom: 30,
-  },
-  button: {
-    backgroundColor: "rgba(255, 255, 255, 0.5)",
-    paddingVertical: 15,
-    paddingHorizontal: 30,
-    borderRadius: 30,
-    marginBottom: 15,
-  },
-  buttonText: {
-    fontSize: 18,
-    color: "black",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-});
