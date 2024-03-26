0c3e0eeac98271991e994178bd0f67be791d6837
Dovydas Šiurkus <dovydas.siurkus@ktu.edu>
2024-03-26 20:58:36 +0200

    Capitalized letter and removed centering of header

--- src/app/(settings)/_layout.tsx
+++ src/app/(settings)/_layout.tsx
@@ -3,14 +3,8 @@ import { Stack } from "expo-router";
 export default function SettingsLayout() {
   return (
     <Stack>
-      <Stack.Screen
-        name="theme"
-        options={{ title: "Theme Settings", headerTitleAlign: "center" }}
-      />
-      <Stack.Screen
-        name="about"
-        options={{ title: "About us", headerTitleAlign: "center" }}
-      />
+      <Stack.Screen name="theme" options={{ title: "Theme Settings" }} />
+      <Stack.Screen name="about" options={{ title: "About Us" }} />
     </Stack>
   );
 }
