5143fa99c616b38ae5f71cb28d2ae785eb337988
OpelShniopel <nn8gs76kuegj1oiwpnk8mjmssycby4@bots.bitbucket.org>
2024-03-30 20:16:13 +0200

    Made so you can see profile picture if url is provided

--- src/app/(tabs)/userInfo.tsx
+++ src/app/(tabs)/userInfo.tsx
@@ -13,11 +13,6 @@ import { useAuth } from "@/providers/AuthProvider";
 
 export default function UserInfoTab() {
   const { profile } = useAuth();
-  const user = {
-    profilePicture: require("../../assets/images/profilePicture.png"),
-    otherData: "Plan/description/etc.",
-  };
-
   const router = useRouter();
 
   const handleEditProfile = () => {
@@ -50,18 +45,25 @@ export default function UserInfoTab() {
         <View className="flex-grow items-center justify-center">
           <View className="items-center mt-20 mb-5">
             <View className="w-32 h-32 flex items-center justify-center rounded-full mb-4 shadow-lg bg-white border-4 border-black">
-              <Image
-                source={user.profilePicture}
-                className="w-28 h-28 rounded-full"
-              />
+              {profile && profile.avatar_url ? (
+                <Image
+                  source={{ uri: profile.avatar_url }}
+                  className="w-28 h-28 rounded-full"
+                />
+              ) : (
+                <Image
+                  source={require("../../assets/images/profilePicture.png")}
+                  className="w-28 h-28 rounded-full"
+                />
+              )}
             </View>
             {profile && profile.username ? (
               <Text className="text-lg font-bold mb-2">{profile.username}</Text>
             ) : (
               <Text className="text-lg font-bold mb-2">Loading...</Text>
             )}
-            <Text>{user.otherData}</Text>
           </View>
+
           <View className="justify-center m-10 w-screen p-5">
             <TouchableOpacity
               className="py-4 px-6 bg-yellow-200 rounded-xl border-2 border-black mb-4"
@@ -87,7 +89,6 @@ export default function UserInfoTab() {
             >
               <Text>Theme Settings</Text>
             </TouchableOpacity>
-
             <TouchableOpacity
               onPress={handleAboutUs}
               className="py-4 px-6 bg-yellow-200 rounded-xl border-2 border-black mb-4"
@@ -95,6 +96,7 @@ export default function UserInfoTab() {
               <Text>About Us</Text>
             </TouchableOpacity>
           </View>
+
           <TouchableOpacity
             onPress={handleLogout}
             className="py-4 px-6 rounded-md w-1/3 mb-10"
